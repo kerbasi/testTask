252fbdc94dda338c6a8501da9573d3ddbe0f8176
Kirill <kirill.novikov@gmail.com>
2022-12-16 14:52:35 +0300

    Added auto carriers list captition

--- script.js
+++ script.js
@@ -9,23 +9,17 @@
   responseData = responseData.result.flights;
   responseData.forEach(({ flight }) => {
     carriers[flight.carrier.uid] = flight.carrier.caption;
-    // const carrierCheckEl = document.createElement("div");
-    // carrierCheckEl.classList.add("aside-form__checkbox");
-    // carrierCheckEl.innerHTML = `
-    //     <input class="aside-form__checkbox" type="checkbox" name="aside-form__radio-carrier" id="check-carrier_${index}">
-    //     <label class="aside-form__label-inline" for="check_1">${flight.carrier.caption}</label>
-    // `;
-    // carrierCheckGroup.appendChild(carrierCheckEl);
   });
-  // carriers.forEach((carrier, index) =>
+
   let index = 0;
   for (const carrier in carriers) {
     const carrierCheckEl = document.createElement("div");
     carrierCheckEl.classList.add("aside-form__checkbox");
     carrierCheckEl.innerHTML = `
         <input class="aside-form__checkbox" type="checkbox" name="aside-form__radio-carrier" id="check-carrier_${index}">
-        <label class="aside-form__label-inline" for="check_1">${carrier.caption}</label>
+        <label class="aside-form__label-inline" for="check_1">${carriers[carrier]}</label>
     `;
+    carrierCheckGroup.appendChild(carrierCheckEl);
     index++;
   }
 }
