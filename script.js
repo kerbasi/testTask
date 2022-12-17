fdb0fdff64365b82bc181eea9a549a6cd90c2226
Kirill Novikov <kirill.novikov@gmail.com>
2022-12-17 10:33:00 +0300

    Added carriers with min price to the form

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-﻿async function getFlightsData() {
+﻿const url = "./data/flights.json";
+
+async function getFlightsData(url) {
+  const response = await fetch(url);
+  let responseData = await response.json();
+  responseData = responseData.result.flights;
+  console.log(responseData);
+  getCarriersRadio(responseData);
+}
+
+function getCarriersRadio(flights) {
   const carrierCheckGroup = document.querySelector(
     ".aside-form__item_type_carriers"
   );
   const carriers = {};
-  const url = "./flights.json";
-  const response = await fetch(url);
-  let responseData = await response.json();
-  responseData = responseData.result.flights;
-  responseData.forEach(({ flight }) => {
-    carriers[flight.carrier.uid] = flight.carrier.caption;
+  flights.forEach(({ flight }) => {
+    if (carriers.hasOwnProperty(flight.carrier.uid)) {
+      carriers[flight.carrier.uid].price.push(flight.price.total.amount);
+    } else {
+      carriers[flight.carrier.uid] = {
+        caption: flight.carrier.caption,
+        price: [flight.price.total.amount],
+      };
+    }
   });
-
+  console.log(carriers);
   let index = 0;
   for (const carrier in carriers) {
     const carrierCheckEl = document.createElement("div");
-    carrierCheckEl.classList.add("aside-form__checkbox");
+    carrierCheckEl.classList.add("aside-form__checkbox-carriers");
     carrierCheckEl.innerHTML = `
         <input class="aside-form__checkbox" type="checkbox" name="aside-form__radio-carrier" id="check-carrier_${index}">
-        <label class="aside-form__label-inline" for="check_1">${carriers[carrier]}</label>
+        <label class="aside-form__label-inline" for="check_1"><span class="aside__carrier-name">${
+          carriers[carrier].caption
+        }</span> от ${
+      carriers[carrier].price.sort((a, b) => a - b)[0]
+    } р.</label>
     `;
     carrierCheckGroup.appendChild(carrierCheckEl);
     index++;
   }
 }
 
-getFlightsData();
+getFlightsData(url);
